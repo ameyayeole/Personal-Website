106526bdbf9a13ba8c66838bdab1f69b3a41b109
ameyayeole <yeoleameya8@gmail.com>
2023-01-27 19:28:28 +0530

    minor change

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,10 +5,10 @@ function Contact (){
         <div className="contact" id="contact">
         <h1 className="skillHead">Contact Me</h1>
         <div className="logos">
-        <a className="a" href="www.linkedin.com/in/ameyayeole"><img className="logo"src={require("./images/linkedin.png")} alt=""/></a>
-        <a href="https://mail.google.com/mail/u/0/?hl=en&tf=cm&fs=1&to=yeoleameya8@gmail.com"><img className="logo" src={require("./images/gmail-logo.png")} alt=""/></a>        
-        <a href="https://www.instagram.com/yeoleameya"><img className="logo" src={require("./images/instagram.png")} alt=""/></a>
-        <a href="https://www.github.com/ameyayeole"><img className="logo" src={require("./images/github.png")} alt=""/></a>
+        <a className="a" href="https://www.linkedin.com/in/ameyayeole" target="_blank"><img className="logo"src={require("./images/linkedin.png")} alt=""/></a>
+        <a href="https://mail.google.com/mail/u/0/?hl=en&tf=cm&fs=1&to=yeoleameya8@gmail.com" target="_blank"><img className="logo" src={require("./images/gmail-logo.png")} alt=""/></a>        
+        <a href="https://www.instagram.com/yeoleameya" target="_blank"><img className="logo" src={require("./images/instagram.png")} alt=""/></a>
+        <a href="https://www.github.com/ameyayeole" target="_blank"><img className="logo" src={require("./images/github.png")} alt=""/></a>
         </div>
         </div>
     )
